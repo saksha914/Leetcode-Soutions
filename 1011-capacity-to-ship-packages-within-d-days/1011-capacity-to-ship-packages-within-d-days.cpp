2fa55c132b268f3bd561c7cd024d29e02b136742
saksha914 <118253416+saksha914@users.noreply.github.com>
2023-05-21 01:23:42 +0530

    Time: 94 ms (33.62%), Space: 31.2 MB (45.76%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isvalid(vector<int>& weights, int days,int mid){
+        int tempdays= 1;
+        int count =0;
+        for(int i=0;i<weights.size();i++){
+            count = count+weights[i];
+            
+            if(weights[i] > mid) return false; 
+            if(count > mid){
+                tempdays++;
+                count = weights[i];
+            }
+            if(tempdays > days) return false;
+
+        }
+        if(tempdays > days ) return false;
+        return true;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+
+        int left=weights[0],right=weights[0];
+        //finding left , right for BS
+        for(int i=1;i<weights.size();i++){
+            left = max(left,weights[1]);
+            right+= weights[i];
+        }
+
+        while(left<right){
+            int mid = left+(right-left)/2;
+            // if its valid then continue on left side of mid
+            if(isvalid(weights,days,mid) == true){ 
+                right = mid;
+            }
+            else left = mid+1;
+        }
+
+        return right;
+        
+    }
+};
